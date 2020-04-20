616b5b2fb4592d23823a2546239bf7e39aed1f39
KIM HYO RIN <wwhurin0515@gmail.com>
2020-04-20 23:47:23 +0900

    add style todo list and remove button

--- 02_exam_project/vue-todo/src/components/TodoList.vue
+++ 02_exam_project/vue-todo/src/components/TodoList.vue
@@ -1,7 +1,17 @@
 <template>
     <section>
         <ul>
-            <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+            <!--
+                 v-for을 사용할 때, key 값이 요구
+                 v-bind:key 추가 (예제에는 나와있지 않음!)
+             -->
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
         </ul>
     </section>
 </template>
@@ -18,10 +28,42 @@ export default {
               this.todoItems.push(localStorage.key(i));
           }
       }
+  }, methods: {
+      removeTodo(todoItem, index) {
+          console.log(todoItem, index);
+      }
   }
 }
 </script>
 
-<style>
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
 
+li {
+    display: flex; 
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
 </style>
