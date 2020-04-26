ac6783b503ab4b90e213f2ffe605dff0870343e6
KIM HYO RIN <wwhurin0515@gmail.com>
2020-04-26 22:44:34 +0900

    add bootstrap

--- 03_etc/src/App.vue
+++ 03_etc/src/App.vue
@@ -2,13 +2,21 @@
   <div id="app">
     App 컴포넌트
     <DatePicker></DatePicker>
+    <hr/>
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
   </div>
 </template>
 
 <script>
 import DatePicker from "./DatePicker.vue"
 export default {
-  components: {
+  name: "app"
+  , data() {
+    return {
+      msg: "Welcome to Your Vue.js"
+    }
+  }
+  , components: {
     DatePicker
   }
   , mounted(){
